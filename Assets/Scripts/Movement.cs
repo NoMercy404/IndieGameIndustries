8e732599cccbd79337cbf088614ab7b6a54ab575
Jasiu1337 <119540784+Jasiu1337@users.noreply.github.com>
2025-03-17 21:57:03 +0100

    Test (#7)
    Test commita
    
    Co-authored-by: KrystianWasil <151478479+KrystianWasil@users.noreply.github.com>
    Co-authored-by: Maciej Wawer <mwawer268@gmail.com>

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+    public LayerMask solidObjectsLayer;
+    
+
+    private Vector2 input;
+    
+    private void Awake()
+    {
+        
+    }
+    private void Update()
+    {
+        HandleUpdate();
+    }
+    public static class GameData
+    {
+        public static Vector3 PlayerPosition;
+    }
+
+    public void HandleUpdate()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+
+
+            if (input != Vector2.zero)
+            {
+                
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if (isWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
+            }
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Pozycja gracza: "+transform.position);
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+
+        isMoving = false;
+
+        CheckForEncounters();
+    }
+    private bool isWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) != null)
+            return false;
+        else return true;
+    }
+
+   
+    private void CheckForEncounters()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.1f, solidObjectsLayer) != null)
+        {
+            if (Random.Range(1, 100) <= 25)
+            {
+                Debug.Log("Zadzialalo");
+            }
+        }
+    }
+}
