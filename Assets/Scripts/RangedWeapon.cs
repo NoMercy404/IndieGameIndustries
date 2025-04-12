9c62b8bd05852362588eb8268cd81f9ce37f4816
Jasiu1337 <jan.trojnar@op.pl>
2025-04-12 14:16:59 +0200

    brońka szczela
    ghee

--- Assets/Scripts/RangedWeapon.cs
+++ Assets/Scripts/RangedWeapon.cs
@@ -31,10 +31,13 @@ public class RangedWeapon : Weapon
     private void Update()
     {
         // Handles the weapon rotation
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle+offset, Vector3.forward);
+        //Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+       // transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
         if (timeBtwShots <= 0)
         {
             if (Input.GetMouseButton(0))
